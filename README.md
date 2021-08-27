c7320fdf50e0a9b68d44d331e93b644f728fef82
Tanner <thetannerthomas@gmail.com>
2021-08-26 18:47:55 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sveltely
+
+Sveltely is a UI library built exclusively for Svelte. The goal is to create completely 
+customizable components to integrate beautifully into any Svelte project.
+
+
+## Installation
+
+To get started you will need to install `sveltely` and its dependencies.
+
+```bash
+  npm install sveltely
+```
+
+... currently under development. Stay tuned.
+
+    
---
+++ packages/Button/Button.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let variant = "primary";
+</script>
+
+<style>
+	button {
+	  color: white;
+	  border: none;
+	  padding: 1rem 3rem;
+	  border-radius: 40px;
+	  letter-spacing: 0.75px;
+	  transition: 0.3s;
+	  cursor: pointer;
+	  font-family: "Poppins";
+	}
+
+	.primary {
+	  background: #5f2eea;
+	}
+	.primary:hover {
+	  background: #2a00a2;
+	}
+	.primary:focus {
+	  outline: none;
+	  box-shadow: 0 0 0 6px #e4daff;
+	}
+
+	.secondary {
+	  background: none;
+	  border: 2px solid #5f2eea;
+	  color: #5f2eea;
+	  font-weight: bold;
+	}
+	.secondary:hover {
+	  border: 2px solid #2a00a2;
+	  color: #2a00a2;
+	}
+	.secondary:focus {
+	  outline: none;
+	  box-shadow: 0 0 0 6px #eff0f6;
+	}
+</style>
+
+
+
+<button class={variant}>
+  <slot></slot>
+</button>
